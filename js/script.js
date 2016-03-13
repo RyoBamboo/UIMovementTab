6f8843decacfc67f198a8fd7a8339add0e62bbf2
ryo takenoshita <itakedaka@gmail.com>
2016-03-14 00:05:32 +0900

    画像を表示させる際にフェードインさせながら表示するように修正

--- js/script.js
+++ js/script.js
@@ -8,16 +8,30 @@ xhr.onload = function(e) {
 		var src = $(this).find('img').data('src'); // gifソースの取得
 		data.push({ url: url, src: src });
 	});
-	show();
-	fadeOut('loader-bg');
+	fadeOut('loader-bg'); // ロード画面を非表示
+	show(); // 画像の表示
 };
 xhr.send();
 
 function show() {
 	$(data).each(function() {
 		var image = "<img src='"+ this.src + "' data-src='"+ this.src + "'>";
-		var link = "<a href='http://uimovement.com"+this.url+"' target='_blank'>" + image +"</a>";
+		var link = "<a class='gif-image' href='http://uimovement.com"+this.url+"' target='_blank' style='display:none'>" + image +"</a>";
 		$('#images').append(link);
+	});
+	fadeIn('gif-image', 500, 1000);
+}
+
+/**
+ * 読み込んだ画像をフェードインさせながら表示する
+ *
+ * elementClass(string): 表示させる要素につけたClass名
+ * delaySpeed(int): フェードインさせるまでのスピード
+ * fadeSpeed(int): フェードインが完了するまでのスピード
+ */
+function fadeIn(elementClass, delaySpeed, fadeSpeed) {
+	$('.'+elementClass).each(function(i) {
+		$(this).delay(i*delaySpeed+delaySpeed).css({display:'block', opacity: 0}).animate({opacity:'1'}, fadeSpeed);
 	});
 }
 
